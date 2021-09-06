ff01f4ab7f5b689f32fbc60b52e35cb2349a8e21
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2021-09-06 19:28:04 -0400

    Added strict(er) password validation

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const validator = require('password-validator');
+
 const User = require('../models/user');
 const { forwardAuthenticated } = require('../config/auth');
 
@@ -10,22 +12,43 @@ router.get('/register', forwardAuthenticated, (req, res) =>
   res.render('register', { user: req.user })
 );
 
+const schema = new validator();
+schema
+.is().min(6)      
+.has().uppercase()
+.has().lowercase()
+.has().symbols()
+
 // POST register
 router.post('/register', (req, res) => {
-  const { name, email, password, password2 } = req.body;
   let errors = [];
-  if (!name || !email || !password || !password2) {
-    errors.push({ msg: 'Please enter all fields' });
+  const { name, email, password, repeatedPassword } = req.body;
+  console.log(repeatedPassword)
+  if (!name || !email || !password || !repeatedPassword) {
+    errors.push({ msg: 'Please fill out all fields' });
+  } else {
+    if (password != repeatedPassword) {
+      errors.push({ msg: 'Passwords do not match' });
+    } else {
+      const validationErrors = schema.validate(password, { list: true });
+      if (validationErrors.length) {
+        for (let errorIdx = 0; errorIdx < validationErrors.length; errorIdx++) {
+          if (validationErrors[errorIdx] == 'min') {
+            errors.push({ msg: 'Password must be at least 6 characters' });
+          } else if (validationErrors[errorIdx] == 'uppercase') {
+            errors.push({ msg: 'Password must have uppercase characters' });
+          } else if (validationErrors[errorIdx] == 'lowercase') {
+            errors.push({ msg: 'Password must have lowercase characters' });
+          } else {
+            errors.push({ msg: 'Password must have symbols (@, !, &, ...)' });
+          }
+        }
+      }
+    }
   }
-  if (password != password2) {
-    errors.push({ msg: 'Passwords do not match' });
-  }
-  if (password.length < 6) {
-    errors.push({ msg: 'Password must be at least 6 characters' });
-  }
-  if (errors.length > 0) {
+  if (errors.length) {
     res.render('register', { 
-      errors, name, email, password, password2, user: req.user
+      errors, name, email, password, repeatedPassword, user: req.user
     });
   } else {
     User.findOne({ email: { "$regex": "^" + email + "\\b", "$options": "i" }})
@@ -33,7 +56,7 @@ router.post('/register', (req, res) => {
       if (user) {
         errors.push({ msg: 'Email already exists' });
         res.render('register', { 
-          errors, name, email, password, password2, user: req.user
+          errors, name, email, password, repeatedPassword, user: req.user
         });
       } else {
         const newUser = new User({ name, email, password });
@@ -43,7 +66,7 @@ router.post('/register', (req, res) => {
             newUser.password = hash;
             newUser.save()
               .then(user => {
-                req.flash('success', 'You are now registered and can log in');
+                req.flash('success', 'You are now registered and can log in!');
                 res.redirect('/');
               })
               .catch(err => console.log(err));
